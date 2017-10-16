48f59d7932fd58b95b7aad9ed957a6d4e9efd87e
didierberlioz <didier.berlioz@agence-soon.fr>
2017-10-16 23:29:52 +0200

    add test

--- public/index.php
+++ public/index.php
@@ -1,4 +1,5 @@
 <?php
+echo 'test'; exit;
 
 // When executed in php built ib server, index.php is the router script and should give all away other requests
 if (preg_match('/\.(?:png|jpg|jpeg|gif|html|css|js|ico)$/', $_SERVER["REQUEST_URI"])) {
