ad809c73912cff3b4b72faab4a44525904ac2f39
didierberlioz <didier.berlioz@agence-soon.fr>
2017-10-16 23:32:55 +0200

    fix root

--- public/index.php
+++ public/index.php
@@ -1,12 +1,11 @@
 <?php
-echo 'test'; exit;
 
 // When executed in php built ib server, index.php is the router script and should give all away other requests
 if (preg_match('/\.(?:png|jpg|jpeg|gif|html|css|js|ico)$/', $_SERVER["REQUEST_URI"])) {
     return false;
 }
 
-const ROOT = '';
+const ROOT = '../';
 
 require ROOT . 'vendor/autoload.php';
 
